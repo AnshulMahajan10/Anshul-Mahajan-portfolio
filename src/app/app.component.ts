f9a7b5987f3b093817519f0baffc63e3b09b000d
Anshul <anshul.mahajan@soliscare.com>
2020-04-08 02:04:49 -0400

    04-08

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio-Anshul';
+
+
+  openFile()
+  {
+    window.open("./Anshul-Mahajan-Resume.pdf");
+  }
 }
